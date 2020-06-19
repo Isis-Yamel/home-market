9fc3b520ff3366c9baf5ef1af2d7d61f55ac24ff
Isis-Yamel <isis.salamanca@globant.com>
2020-06-19 18:43:38 -0500

    set up redux and folder structure

--- src/index.js
+++ src/index.js
@@ -6,12 +6,23 @@ import { IntlProvider } from 'react-intl';
 import { locales } from './i18n/locales';
 import messages from './i18n/messages';
 
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer,(composeEnhancers(applyMiddleware(thunk))));
+
 const locale = locales.SPANISH;
 
 ReactDOM.render(
-  <IntlProvider locale={locale} textComponent={Fragment} messages={messages[locale]}>
-    <App />
-  </IntlProvider>,
+  <Provider store={store}>
+      <IntlProvider locale={locale} textComponent={Fragment} messages={messages[locale]}>
+        <App />
+      </IntlProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
